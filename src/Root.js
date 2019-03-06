67bac389bbf6f720b36d398c6d87d63d0199021c
ulyana-spiridonova <ulyana.spiridonova@tesseris.com>
2019-03-06 16:36:35 +0200

    add middleware for redux-promise

--- src/Root.js
+++ src/Root.js
@@ -2,13 +2,9 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "reducers";
-import reduxPromise from "redux-promise";
+import async from "middleware/async";
 
 export default ({ children, initialState = {} }) => {
-    const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
-    return (
-        <Provider store={store}>
-            {children}
-        </Provider>
-    )
-}
+  const store = createStore(reducers, initialState, applyMiddleware(async));
+  return <Provider store={store}>{children}</Provider>;
+};
---
+++ src/middleware/async.js
@@ -0,0 +1,12 @@
+export default ({ dispatch }) => next => action => {
+  debugger;
+
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+
+  action.payload.then(response => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
+};
