f0aac6cb685c16b0ae95fdd8b706c8fa5f4f76d8
ulyana-spiridonova <ulyana.spiridonova@gmail.com>
2019-03-06 17:06:54 +0200

    styling

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from "actions/types";
 
-export const saveComment = (comment) => {
-    return {
-        type: SAVE_COMMENT,
-        payload: comment
-    }
+export const saveComment = comment => {
+  return {
+    type: SAVE_COMMENT,
+    payload: comment
+  };
 };
 
 export const fetchComments = () => {
-    try {
-        const response = axios.get("http://jsonplaceholder.typicode.com/comments");
-        return {
-            type: FETCH_COMMENTS,
-            payload: response
-        }
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const response = axios.get("http://jsonplaceholder.typicode.com/comments");
+    return {
+      type: FETCH_COMMENTS,
+      payload: response
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-export const changeAuth = (isLoggedIn) => {
-    return {
-        type: CHANGE_AUTH,
-        payload: isLoggedIn
-    }
-}
+export const changeAuth = isLoggedIn => {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "Root";
-import {BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from "react-router-dom";
 import App from "components/App";
 
 ReactDOM.render(
-    <Root>
-        <BrowserRouter>
-            <Route path="/" component={App}/>
-        </BrowserRouter>
-    </Root>,
-    document.getElementById('root'));
+  <Root>
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  </Root>,
+  document.getElementById("root")
+);
