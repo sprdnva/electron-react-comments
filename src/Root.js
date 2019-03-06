2c60f9f49ceddf52617fc2502323085917461a10
ulyana-spiridonova <ulyana.spiridonova@gmail.com>
2019-03-06 17:36:14 +0200

    add middleware for state schema validation

--- src/Root.js
+++ src/Root.js
@@ -3,8 +3,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "reducers";
 import async from "middleware/async";
+import stateValidator from "middleware/stateValidator";
 
 export default ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(async, stateValidator)
+  );
   return <Provider store={store}>{children}</Provider>;
 };
